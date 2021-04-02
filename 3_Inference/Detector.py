243567cbec2f19dcee3d29687e02a04c6fe26fb2
vipendra_pal_rajput <vipendrapal98@gmail.com>
2021-04-02 16:18:45 +0530

    Update Detector.py

--- 3_Inference/Detector.py
+++ 3_Inference/Detector.py
@@ -138,8 +138,8 @@ if __name__ == "__main__":
         "--postfix",
         type=str,
         dest="postfix",
-        default="_catface",
-        help='Specify the postfix for images with bounding boxes. Default is "_catface"',
+        default="_labeled_img",
+        help='Specify the postfix for images with bounding boxes. Default is "_labeled_img"',
     )
 
     parser.add_argument(
